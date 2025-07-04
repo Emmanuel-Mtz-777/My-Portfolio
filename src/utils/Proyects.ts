5e8e862056d1dbea672fa6a1e744639059fe034d
Leviek <hemmanuelmtz777@gmail.com>
2025-07-03 21:55:04 -0600

    correction of a grammatical error

--- src/utils/Proyects.ts
+++ src/utils/Proyects.ts
@@ -2,7 +2,7 @@ export type Project = {
     title: string;
     description: string;
     github: string;
-    image: string;
+    image?: string;
     tags: string[];
 };
 
@@ -10,10 +10,17 @@ export type Project = {
 
 export const Projects:Project[] =[
     {
-        title:'titulo',
-        description:'',
-        github:'',
+        title:'Spotify-Insights-with-Elasticsearch',
+        description:'Proyecto de análisis de Big Data que utiliza Elasticsearch para procesar y consultar un gran dataset de canciones de Spotify. El objetivo es realizar consultas sobre características clave de las canciones, como duración, energía y valencia, y presentar los resultados a través de visualizaciones interactivas en Jupyter Notebooks.',
+        github:'https://github.com/Emmanuel-Mtz-777/Spotify-Insights-with-Elasticsearch',
         image:'',
-        tags:['']
+        tags:['python','Jupyter Notebook']
+    },
+    {
+        title:'C-Ita',
+        description:'Proyecto frontend desarrollado en Kotlin, enfocado en crear una red social tipo Facebook para estudiantes del ITA. Permite la interacción entre usuarios mediante publicaciones, usando APIs y una base de datos SQLite local para simular funcionalidades básicas',
+        github:'https://github.com/Emmanuel-Mtz-777/Cita',
+        image:'',
+        tags:['Kotlin']
     }
 ]
