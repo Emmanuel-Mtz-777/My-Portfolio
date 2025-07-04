2e2ec0da93731fe51c7782ba92abc66cb1097f77
Leviek <hemmanuelmtz777@gmail.com>
2025-07-04 13:19:07 -0600

    added the missing image

--- src/utils/Proyects.ts
+++ src/utils/Proyects.ts
@@ -29,7 +29,7 @@ export const MyProjects:Project[] =[
         title:'C-Ita',
         description:'Proyecto frontend desarrollado en Kotlin, enfocado en crear una red social tipo Facebook para estudiantes del ITA. Permite la interacción entre usuarios mediante publicaciones, usando APIs y una base de datos SQLite local para simular funcionalidades básicas',
         github:'https://github.com/Emmanuel-Mtz-777/Cita',
-        image:'',
+        image:'/images/C-Ita.avif',
         tags:['Kotlin']
     },
 ]
