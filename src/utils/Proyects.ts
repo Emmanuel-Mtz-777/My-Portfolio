0e2bcea3c49cf60e51b475e3081b7bed8cc95d95
Leviek <hemmanuelmtz777@gmail.com>
2025-07-03 21:43:17 -0600

    added a respondive design

---
+++ src/utils/Proyects.ts
@@ -0,0 +1,19 @@
+export type Project = {
+    title: string;
+    description: string;
+    github: string;
+    image: string;
+    tags: string[];
+};
+
+
+
+export const Projects:Project[] =[
+    {
+        title:'titulo',
+        description:'',
+        github:'',
+        image:'',
+        tags:['']
+    }
+]
